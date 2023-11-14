07519d84ff5917ce5f786ba1a8c126680faf19c0
Anushka Venkatesh <114446792+Anushka-Venkatesh@users.noreply.github.com>
2023-11-13 21:05:58 -0800

    Update ProcessList.cpp

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -50,7 +50,7 @@ ProcessList::Result ProcessList::exec()
             char line[128];
             snprintf(line, sizeof(line),
                     "%3d %1d %7d %4d %5d %10s %32s\r\n",
-                     pid, /*retriving priority,*/ info.kernelState.parent,
+                     pid, info.kernelState.priority, info.kernelState.parent,
                      0, 0, *info.textState, *info.command);
             out << line;
         }
