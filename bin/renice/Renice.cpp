61aed509d1cee6563ec21cc70279daa7868b83df
Anushka Venkatesh <114446792+Anushka-Venkatesh@users.noreply.github.com>
2023-11-13 20:05:08 -0800

    Create Renice.cpp
    Bare bones Renice.cpp structure

---
+++ bin/renice/Renice.cpp
@@ -0,0 +1,27 @@
+#include "sys/renice.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "Renice.h"
+#include <ProcessClient.h>
+
+Renice::Renice(int argc, char **argv)
+    : POSIXApplication(argc, argv)
+{
+    parser().setDescription("Changes the scheduling priority of a running process.");
+    parser().registerPositional("PRIORITY", "Change the priority to this level");
+    parser().registerPositional("PROCESS_ID", "Change the priority of this process");
+    parser().registerFlag('n', "priority", "change priority level");
+}
+
+Renice::~Renice()
+{
+}
+
+Renice::Result Renice::exec()
+{ 
+    // Done
+    return Success;
+}
