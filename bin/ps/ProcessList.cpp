8d083c2c5af5992cf7ed344c7935f7ceb914b905
Anushka Venkatesh <114446792+Anushka-Venkatesh@users.noreply.github.com>
2023-11-13 21:08:06 -0800

    Update ProcessList.cpp

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -34,7 +34,7 @@ ProcessList::Result ProcessList::exec()
     String out;
 
     // Print header
-    out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+    out << "ID  PRIORITY  PARENT  USER GROUP STATUS     CMD\r\n";
 
     // Loop processes
     for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
@@ -49,8 +49,8 @@ ProcessList::Result ProcessList::exec()
             // Output a line
             char line[128];
             snprintf(line, sizeof(line),
-                    "%3d %7d %4d %5d %10s %32s\r\n",
-                     pid, info.kernelState.parent,
+                    "%3d %1d %7d %4d %5d %10s %32s\r\n",
+                     pid, info.kernelState.priority, info.kernelState.parent,
                      0, 0, *info.textState, *info.command);
             out << line;
         }
