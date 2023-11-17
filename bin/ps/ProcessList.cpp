d4ccf9512ce24f9337e90d6a5bcfba6b0bee781e
Hailee Wheat <85207784+bopppermon@users.noreply.github.com>
2023-11-16 22:45:54 -0800

    Update ProcessList.cpp
    update

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -33,8 +33,13 @@ ProcessList::Result ProcessList::exec()
     const ProcessClient process;
     String out;
 
-   
-
+    if(arguments().get("priority")) {
+        out << "ID  PARENT  PRIORITY USER GROUP STATUS     CMD\r\n";
+    } else {
+        // Print header
+        out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+    }
+    
     // Loop processes
     for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
     {
@@ -44,28 +49,23 @@ ProcessList::Result ProcessList::exec()
         if (result == ProcessClient::Success)
         {
             DEBUG("PID " << pid << " state = " << *info.textState);
-            if (arguments().get("priority"))
-            {
-                 // Print header
-                out << "ID  PRIORITY  PARENT  USER GROUP STATUS     CMD\r\n";
+
+
+            if(arguments().get("priority")) {
                 // Output a line
                 char line[128];
                 snprintf(line, sizeof(line),
-                        "%3d %8d %7d %4d %5d %10s %32s\r\n",
-                         pid, info.kernelState.priority, info.kernelState.parent,
-                         0, 0, *info.textState, *info.command);
+                        "%3d %7d %8d %4d %5d %10s %32s\r\n",
+                        pid, info.kernelState.parent, info.kernelState.priority,
+                        0, 0, *info.textState, *info.command);
                 out << line;
-            }
-            else
-            {
-                 // Print header
-                out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+            } else {
                 // Output a line
                 char line[128];
                 snprintf(line, sizeof(line),
                         "%3d %7d %4d %5d %10s %32s\r\n",
-                         pid, info.kernelState.parent,
-                         0, 0, *info.textState, *info.command);
+                        pid, info.kernelState.parent,
+                        0, 0, *info.textState, *info.command);
                 out << line;
             }
         }
