1d46250704e2fcb7114ce659c2acb9373e09b496
Hailee Wheat <85207784+bopppermon@users.noreply.github.com>
2023-11-16 21:45:32 -0800

    Update ProcessList.cpp
    Added -l functionality

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -33,8 +33,7 @@ ProcessList::Result ProcessList::exec()
     const ProcessClient process;
     String out;
 
-    // Print header
-    out << "ID  PRIORITY  PARENT  USER GROUP STATUS     CMD\r\n";
+   
 
     // Loop processes
     for (ProcessID pid = 0; pid < ProcessClient::MaximumProcesses; pid++)
@@ -45,14 +44,30 @@ ProcessList::Result ProcessList::exec()
         if (result == ProcessClient::Success)
         {
             DEBUG("PID " << pid << " state = " << *info.textState);
-
-            // Output a line
-            char line[128];
-            snprintf(line, sizeof(line),
-                    "%3d %1d %7d %4d %5d %10s %32s\r\n",
-                     pid, info.kernelState.priority, info.kernelState.parent,
-                     0, 0, *info.textState, *info.command);
-            out << line;
+            if (arguments().get("priority))
+            {
+                 // Print header
+                out << "ID  PRIORITY  PARENT  USER GROUP STATUS     CMD\r\n";
+                // Output a line
+                char line[128];
+                snprintf(line, sizeof(line),
+                        "%3d %8d %7d %4d %5d %10s %32s\r\n",
+                         pid, info.kernelState.priority, info.kernelState.parent,
+                         0, 0, *info.textState, *info.command);
+                out << line;
+            }
+            else
+            {
+                 // Print header
+                out << "ID  PARENT  USER GROUP STATUS     CMD\r\n";
+                // Output a line
+                char line[128];
+                snprintf(line, sizeof(line),
+                        "%3d %7d %4d %5d %10s %32s\r\n",
+                         pid, info.kernelState.parent,
+                         0, 0, *info.textState, *info.command);
+                out << line;
+            }
         }
     }
 
