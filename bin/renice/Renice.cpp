8739b442279964434c5a9bc6fa84ed084be97b4e
Hailee Wheat <iamhaileewheat@gmail.com>
2023-11-17 22:17:00 -0800

    Update Renice.cpp
    ver 2

--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -10,9 +10,9 @@
 Renice::Renice(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
-    parser().setDescription("Changes the scheduling priority of a running process.");
-    parser().registerPositional("PRIORITY", "Change the priority to this level");
-    parser().registerPositional("PROCESS_ID", "Change the priority of this process");
+    parser().setDescription("Alters the scheduling priority of a running process.");
+    parser().registerPositional("PRIORITY", "change the scheduling priority to this level");
+    parser().registerPositional("PROCESS_ID", "change the sceduling priority of this process");
     parser().registerFlag('n', "priority", "change priority level");
 }
 
@@ -21,33 +21,30 @@ Renice::~Renice()
 }
 
 Renice::Result Renice::exec()
-{ 
-    if (arguments().get("priority"))
-   {
-       const ProcessClient process; 
-       ProcessID pid = (atoi(arguments().get("PROCESS_ID")));
-       int priority = (atoi(arguments().get("PRIORITY")));
-       
-       ProcessClient::Info info; 
-       const ProcessClient::Result result = process.processInfo(pid, info); 
+{
+    if(arguments().get("priority")) {
+        const ProcessClient process;
+        ProcessID pid = (atoi(arguments().get("PROCESS_ID")));
+        int priority = (atoi(arguments().get("PRIORITY")));
 
-        // check that the new priority is valid
-        if(result != ProcessClient::Success) 
-        {
+        ProcessClient::Info info;
+        const ProcessClient::Result result = process.processInfo(pid, info);
+
+        // check that the process exists    
+        if(result != ProcessClient::Success) {
             ERROR("No process of ID '" << pid << "' is found")
             return InvalidArgument;
         }
+
         // check that the new priority is valid
-        if(priority >= 1 && priority <= 5) {
-            INFO("Process " << pid << "'s priority has been updated to " << priority);
-            return Success;
-        } 
-        else {
-            ERROR("Invalid priority level. Failed to set priority for process " << pid)
+        if(priority > 5 || priority < 1) {
+            ERROR("Failed to set priority for process " << pid)
             return InvalidArgument;
         }
-       renicepid(pid, priority, 0, 0);
-       printf("process %d set to priority %d, from priority %d\n", pid, priority, info.kernelState.priority);
+
+        renicepid(pid, priority, 0, 0);
+        printf("process %d set to priority %d, from priority %d\n", pid, priority, info.kernelState.priority);
     }
+
     return Success;
 }
